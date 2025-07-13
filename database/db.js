ee851f0030c4b24d70ab2c7d850403d2538f3c84
minnica <minnica20@gmail.com>
2025-07-12 20:03:39 -0600

    feat(*) debug error deploy

--- database/db.js
+++ database/db.js
@@ -2,6 +2,8 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
+console.log('DB_DIALECT (from env):', process.env.DB_DIALECT);
+
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
